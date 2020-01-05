fbb20dc27cb78b224fe7a7444c93689aea46244f
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2020-01-05 00:17:38 -0600

    WhatsApp

--- practica_1/lib/main.dart
+++ practica_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -8,18 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'WhatsApp',
       theme: ThemeData(
-        primarySwatch: MaterialColor(0xFF075E54, <int, Color>{
-          50:  Color(0xFFFFFFFF),
-          100: Color(0xFFFFFFFF),
-          200: Color(0xFFFFFFFF),
-          300: Color(0xFFFFFFFF),
-          400: Color(0xFFFFFFFF),
-          500: Color(0xFFFFFFFF),
-          600: Color(0xFFFFFFFF),
-          700: Color(0xFFFFFFFF),
-          800: Color(0xFFFFFFFF),
-          900: Color(0xFFFFFFFF),
-        }),
+        primaryColor: Color(0xFF075E54),
       ),
       home: MyHomePage(title: 'WhatsApp'),
     );
@@ -34,13 +24,42 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>{
+  var relojsito = true;
+  final _controller = DefaultTabController(length: 3,);
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 5), (timer){
+      setState(() {
+        relojsito = false;
+      });
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    if(relojsito) return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/wa.jpg"),
+            fit: BoxFit.cover
+          )
+        ),
+      )
+    );
     return DefaultTabController(
       length: 3,
       child: Scaffold(
         appBar: AppBar(
+          actions: <Widget>[
+            IconButton(icon: (Icon(Icons.search ,color: Colors.white,)),),
+            IconButton(icon: (Icon(Icons.add_call,color: Colors.white,)),),
+            IconButton(icon: (Icon(Icons.more_vert,color: Colors.white,)),)
+          ],
           title: Text(widget.title),
           bottom: TabBar(
             tabs: <Widget>[
@@ -58,9 +77,288 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: TabBarView(
           children: <Widget>[
-            Container(),
-            Container(),
-            Container()
+            ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://i.ytimg.com/vi/0EsKyNPQt4g/hqdefault.jpg"),
+                      radius: 25,
+                    ),
+                    trailing: Icon(Icons.call),
+                    title: Text("Skeleeer"),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Icon(Icons.call_received, color: Colors.red, size: 15,),
+                        Text(" 23 de Julio, 1:34",style: TextStyle(
+                          color: Colors.black38
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://djanetop.com/upload/djanes/263-madds.jpg"),
+                      radius: 25,
+                    ),
+                    trailing: Icon(Icons.call),
+                    title: Text("Skeleeer"),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Icon(Icons.call_received, color: Colors.red, size: 15,),
+                        Text(" 23 de Julio, 1:34",style: TextStyle(
+                            color: Colors.black38
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://www.metroweekly.com/wp-content/uploads/2019/03/Markus-Notch-Persson-1024x865.jpg"),
+                      radius: 25,
+                    ),
+                    trailing: Icon(Icons.call),
+                    title: Text("Skeleeer"),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Icon(Icons.call_received, color: Colors.red, size: 15,),
+                        Text(" 23 de Julio, 1:34",style: TextStyle(
+                            color: Colors.black38
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://m.media-amazon.com/images/M/MV5BODU5Yzg2YTMtM2ExZS00OTljLTgzOTUtYzg3Zjk2MmJkZjFjXkEyXkFqcGdeQXVyNjEyMTg2OTk@._V1_.jpg"),
+                      radius: 25,
+                    ),
+                    trailing: Icon(Icons.call),
+                    title: Text("Skeleeer"),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Icon(Icons.call_received, color: Colors.red, size: 15,),
+                        Text(" 23 de Julio, 1:35",style: TextStyle(
+                            color: Colors.black38
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://isdb.pw/upload2/319040229/1829792786048663336.jpg"),
+                      radius: 25,
+                    ),
+                    trailing: Icon(Icons.call),
+                    title: Text("Skeleeer"),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Icon(Icons.call_received, color: Colors.red, size: 15,),
+                        Text(" 23 de Julio, 1:36",style: TextStyle(
+                            color: Colors.black38
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                ]
+            ),
+            ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://i.ytimg.com/vi/0EsKyNPQt4g/hqdefault.jpg"),
+                      radius: 25,
+                    ),
+                    trailing: Text("8:05",style: TextStyle(color: (Colors.black38)),),
+                    title: Text("Skeleeer"),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Icon(Icons.check, color: Colors.black38, size: 15,),
+                        Text(" Hola profe, si ve esto, pongame 10",style: TextStyle(
+                            color: Colors.black38
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://djanetop.com/upload/djanes/263-madds.jpg"),
+                      radius: 25,
+                    ),
+                    trailing: Text("8:05",style: TextStyle(color: (Colors.black38)),),
+                    title: Text("Skeleeer"),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Icon(Icons.check, color: Colors.black38, size: 15,),
+                        Text(" Hola profe, si ve esto, pongame 10",style: TextStyle(
+                            color: Colors.black38
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://www.metroweekly.com/wp-content/uploads/2019/03/Markus-Notch-Persson-1024x865.jpg"),
+                      radius: 25,
+                    ),
+                    trailing: Text("8:05",style: TextStyle(color: (Colors.black38)),),
+                    title: Text("Skeleeer"),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Icon(Icons.check, color: Colors.black38, size: 15,),
+                        Text(" Hola profe, si ve esto, pongame 10",style: TextStyle(
+                            color: Colors.black38
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://m.media-amazon.com/images/M/MV5BODU5Yzg2YTMtM2ExZS00OTljLTgzOTUtYzg3Zjk2MmJkZjFjXkEyXkFqcGdeQXVyNjEyMTg2OTk@._V1_.jpg"),
+                      radius: 25,
+                    ),
+                    trailing: Text("8:05",style: TextStyle(color: (Colors.black38)),),
+                    title: Text("Skeleeer"),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Icon(Icons.check, color: Colors.black38, size: 15,),
+                        Text(" Hola profe, si ve esto, pongame 10",style: TextStyle(
+                            color: Colors.black38
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://isdb.pw/upload2/319040229/1829792786048663336.jpg"),
+                      radius: 25,
+                    ),
+                    trailing: Text("8:05",style: TextStyle(color: (Colors.black38)),),
+                    title: Text("Skeleeer"),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Icon(Icons.check, color: Colors.black38, size: 15,),
+                        Text(" Hola profe, si ve esto, pongame 10",style: TextStyle(
+                            color: Colors.black38
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                ]
+            ),
+            ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://i.ytimg.com/vi/0EsKyNPQt4g/hqdefault.jpg"),
+                      radius: 25,
+                    ),
+                    trailing: Text("Móvil",style: TextStyle(color: (Colors.black38)),),
+                    title: Text("Skeleeer"),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+
+                        Text(" Disponible",style: TextStyle(
+                            color: Colors.black38
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://djanetop.com/upload/djanes/263-madds.jpg"),
+                      radius: 25,
+                    ),
+                    trailing: Text("Móvil",style: TextStyle(color: (Colors.black38)),),
+                    title: Text("Skeleeer"),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+
+                        Text(" Disponible",style: TextStyle(
+                            color: Colors.black38
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://www.metroweekly.com/wp-content/uploads/2019/03/Markus-Notch-Persson-1024x865.jpg"),
+                      radius: 25,
+                    ),
+                    trailing: Text("Móvil",style: TextStyle(color: (Colors.black38)),),
+                    title: Text("Skeleeer"),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+
+                        Text(" Disponible",style: TextStyle(
+                            color: Colors.black38
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://m.media-amazon.com/images/M/MV5BODU5Yzg2YTMtM2ExZS00OTljLTgzOTUtYzg3Zjk2MmJkZjFjXkEyXkFqcGdeQXVyNjEyMTg2OTk@._V1_.jpg"),
+                      radius: 25,
+                    ),
+                    trailing: Text("Móvil",style: TextStyle(color: (Colors.black38)),),
+                    title: Text("Skeleeer"),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+
+                        Text(" Disponible",style: TextStyle(
+                            color: Colors.black38
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage("https://isdb.pw/upload2/319040229/1829792786048663336.jpg"),
+                      radius: 25,
+                    ),
+                    trailing: Text("Móvil",style: TextStyle(color: (Colors.black38)),),
+                    title: Text("Skeleeer"),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+
+                        Text(" Disponible",style: TextStyle(
+                            color: Colors.black38
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                ]
+            ),
           ],
         ),
       ),
